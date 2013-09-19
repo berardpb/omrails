261bdd3e5bfaf6d22281d2c027ca18e53aaad092
Patrick B. Berarducci <berardpb22@gmail.com>
2013-09-19 07:04:23 -0400

    Add Pins Association with users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,7 @@ class Pin < ActiveRecord::Base
 
 	# other types of validations at rails validation site
 	validates :description, presence: true 
+
+	belongs_to :user
+	validates :user_id, presence: true
 end
